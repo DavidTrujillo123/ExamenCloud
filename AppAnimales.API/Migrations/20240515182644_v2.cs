799122f44620a2e9ed25f7a1d49f99edfcf7e778
DAVID TRUJILLO <davot098@gmail.com>
2024-05-15 15:00:59 -0500

    Renombre de carpetas

---
+++ AppAnimales.API/Migrations/20240515182644_v2.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppAnimales.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animales", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grupos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Caracteristica = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grupos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animales");
+
+            migrationBuilder.DropTable(
+                name: "Grupos");
+        }
+    }
+}
---
+++ AppAnimales.API/Migrations/20240515182907_v3.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppAnimales.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class v3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GrupoId",
+                table: "Animales",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animales_GrupoId",
+                table: "Animales",
+                column: "GrupoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Animales_Grupos_GrupoId",
+                table: "Animales",
+                column: "GrupoId",
+                principalTable: "Grupos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Animales_Grupos_GrupoId",
+                table: "Animales");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Animales_GrupoId",
+                table: "Animales");
+
+            migrationBuilder.DropColumn(
+                name: "GrupoId",
+                table: "Animales");
+        }
+    }
+}
